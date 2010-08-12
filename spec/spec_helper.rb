36b1086b29df71c222ee5a76cddbde43dbd11aa7
Bill Siggelkow <bsiggelkow@mac.com>
2010-08-12 10:54:05 -0400

    [BS][000000] Added require of rubygems to spec helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,7 @@
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'app', 'controllers'))
+require 'rubygems'
 require 'ops_routes'
 require 'ops_routes/middleware'
 
