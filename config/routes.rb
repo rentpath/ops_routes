d3401c8b965143372f6d07b5f651b1cf7e65a0d1
Emily Price <price.emily@gmail.com>
2010-10-12 09:40:43 -0400

    [EP] Make routing compatible with Rails 3

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
-ActionController::Routing::Routes.draw do |map|
-  map.connect '/ops/version', :controller => 'ops', :action => 'version'
-  map.connect '/ops/heartbeat', :controller => 'ops', :action => 'heartbeat'
-  map.connect '/ops/heartbeat/:name', :controller => 'ops', :action => 'heartbeat'
-  map.connect '/ops/configuration', :controller => 'ops', :action => 'configuration'
+if Rails::VERSION::MAJOR == 3
+  Rails.application.routes.draw do |map|
+    map '/ops/version', :to => 'ops#version'
+    map '/ops/heartbeat(/:name)', :to => 'ops#heartbeat'
+    map '/ops/configuration', :to => 'ops#configuration'
+  end
+else  
+  ActionController::Routing::Routes.draw do |map|
+    map.connect '/ops/version', :controller => 'ops', :action => 'version'
+    map.connect '/ops/heartbeat', :controller => 'ops', :action => 'heartbeat'
+    map.connect '/ops/heartbeat/:name', :controller => 'ops', :action => 'heartbeat'
+    map.connect '/ops/configuration', :controller => 'ops', :action => 'configuration'
+  end
 end
